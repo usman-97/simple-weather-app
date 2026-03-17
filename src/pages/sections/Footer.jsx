a9ac059313bd6d6acc5f0f807038aa6d70a1324c
usman-97 <usmanshabir65@hotmail.com>
2026-03-17 18:03:00 +0000

    Re-style footer

--- src/pages/sections/Footer.jsx
+++ src/pages/sections/Footer.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center space-y-3 w-full p-5 bg-custom-black text-off-white text-sm leading-6 md:text-2xl">
-      <span>&copy; Usman Shabir</span>
-      <span>
+    <div className="flex flex-col items-center space-y-2 w-full p-6 bg-custom-black text-sm leading-6">
+      <span className="text-gray-200 tracking-widest md:text-2xl">
+        &copy; {new Date().getFullYear()} Usman Shabir
+      </span>
+      <span className="text-gray-400 md:text-lg">
         Powered by&nbsp;
         <a
           href="https://www.weatherapi.com/"
           title="Weather API"
-          className="font-light underline"
+          className="font-light underline hover:text-[#5ed3ff]!"
           target="_blank"
         >
           WeatherAPI.com
